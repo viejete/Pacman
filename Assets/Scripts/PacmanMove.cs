89887589a11171910be3dd04d205b1da6dcd89e3
Carlos <carlosramnieto@gmail.com>
2017-05-02 11:38:43 +0200

    Aprovat...

---
+++ Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PacmanMove : MonoBehaviour {
+
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        dest = transform.position;
+	}
+	
+	void FixedUpdate () {
+        // Move closer to destination
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        // Check for Input if not moving
+        if ((Vector2)transform.position == dest)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+                dest = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+                dest = (Vector2)transform.position + Vector2.right;
+            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+                dest = (Vector2)transform.position - Vector2.up;
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+                dest = (Vector2)transform.position - Vector2.right;
+        }
+
+        // Animation parameters
+        Vector2 dir = dest - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+
+        // Press R to restart
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("scene");
+        }
+	}
+
+    bool valid(Vector2 dir)
+    {
+        // Cast Line from next to Pacman to pacman
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
